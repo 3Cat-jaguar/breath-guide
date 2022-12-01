ce6e0bef025c3a6e2a337e0aadf9c9e63ccac4a6
YuriLee <yuri.lee@sisoul.co.kr>
2022-12-01 17:57:08 +0900

    Feature resize animation for breath guide
    - resize circle's radius
    - zoom in/out 4sec each
    - change alpha 100% to 30%
    - without animation xml

--- app/src/main/java/kr/co/cat_diversity/ylee/breath_guide/MainActivity.kt
+++ app/src/main/java/kr/co/cat_diversity/ylee/breath_guide/MainActivity.kt
@@ -1,7 +1,13 @@
 package kr.co.cat_diversity.ylee.breath_guide
 
+import android.icu.number.Scale
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AccelerateDecelerateInterpolator
+import android.view.animation.AlphaAnimation
+import android.view.animation.Animation
+import android.view.animation.AnimationSet
+import android.view.animation.ScaleAnimation
 import kr.co.cat_diversity.ylee.breath_guide.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +16,58 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding.guideCircle.startAnimation(getAnimationSet())
+
         setContentView(binding.root)
+    }
+
+    private fun getAnimationSet() : AnimationSet {
+        val animationSet : AnimationSet = AnimationSet(true)
+        animationSet.interpolator = AccelerateDecelerateInterpolator()
+
+        val animationDuration = 4000L
+        animationSet.addAnimation(getZoomInAnimation(0.5f, 3f, animationDuration))
+        animationSet.addAnimation(getAlphaAnimation(1f, 0.3f, animationDuration))
+
+        return animationSet
+    }
+
+    private fun getZoomInAnimation(
+        fromSize : Float, toSize : Float, duration : Long,
+        repeatCount : Int = Animation.INFINITE,
+        repeatMode : Int = Animation.REVERSE
+    ) : Animation {
+        val zoomInAnimation : Animation = ScaleAnimation(
+            fromSize,
+            toSize,
+            fromSize,
+            toSize,
+            Animation.RELATIVE_TO_SELF,
+            0.5f,
+            Animation.RELATIVE_TO_SELF,
+            0.5f
+        )
+
+        zoomInAnimation.duration = duration
+        zoomInAnimation.repeatCount = repeatCount
+        zoomInAnimation.repeatMode = repeatMode
+
+        return zoomInAnimation
+    }
+
+    private fun getAlphaAnimation(
+        fromAlpha : Float, toAlpha : Float, duration : Long,
+        repeatCount : Int = Animation.INFINITE,
+        repeatMode : Int = Animation.REVERSE
+    ) : Animation {
+        val alphaAnimation : Animation = AlphaAnimation(
+            fromAlpha, toAlpha
+        )
+        alphaAnimation.duration = duration
+        alphaAnimation.repeatCount = repeatCount
+        alphaAnimation.repeatMode = repeatMode
+
+        return alphaAnimation
     }
 }
